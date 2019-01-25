70ee0d4e7301c3e09e721ae4141a8ffed79f75e9
hoshianaaa <40942409+hoshianaaa@users.noreply.github.com>
2019-01-25 22:47:47 +0900

    Create main.c

---
+++ master/main.c
@@ -0,0 +1,32 @@
+#include <Wire.h>
+
+int cb = 0;
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  Wire.requestFrom(8, 2);// request 1 bytes from Slave ID #8
+
+  while (Wire.available()) {
+    byte b = Wire.read();
+    byte c = Wire.read();
+    
+    
+
+    cb |= b;
+    cb <<= 8;
+    cb |= c;
+
+   
+
+    Serial.println(cb);
+     cb = 0;
+    
+    Serial.println("hello");
+  }
+  Serial.println("he");
+
+  delay(500);
+}
